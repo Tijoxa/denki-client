1431ad072a5d9c99502b3023d735dda263fecdd9
Tijoxa <tijoxa@gmail.com>
2025-10-16 21:13:53 +0200

    Update schemas.py

--- src/denki_client/schemas.py
+++ src/denki_client/schemas.py
@@ -7,7 +7,7 @@ from denki_client.area import BusinessType, FlowDirection, PsrType
 DAY_AHEAD_SCHEMA = nw.Schema(
     {
         "timestamp": nw.Datetime(time_zone=UTC),
-        "price.amount": nw.Float64,
+        "price.amount": nw.Float64(),
         "currency_Unit.name": nw.Enum(["EUR"]),
         "price_Measure_Unit.name": nw.Enum(["MWH"]),
         "resolution": nw.Enum(["PT60M", "PT30M", "PT15M"]),
@@ -17,7 +17,7 @@ DAY_AHEAD_SCHEMA = nw.Schema(
 ACTIVATED_BALANCING_ENERGY_PRICES_SCHEMA = nw.Schema(
     {
         "timestamp": nw.Datetime(time_zone=UTC),
-        "activation_Price.amount": nw.Float64,
+        "activation_Price.amount": nw.Float64(),
         "flowDirection.direction": nw.Enum([key.name for key in FlowDirection]),
         "businessType": nw.Enum([key.name for key in BusinessType]),
         "currency_Unit.name": nw.Enum(["EUR"]),
@@ -29,7 +29,7 @@ ACTIVATED_BALANCING_ENERGY_PRICES_SCHEMA = nw.Schema(
 ACTUAL_GENERATION_PER_GENERATION_UNIT = nw.Schema(
     {
         "timestamp": nw.Datetime(time_zone=UTC),
-        "quantity": nw.Int64,
+        "quantity": nw.Int64(),
         "quantity_Measure_Unit.name": nw.Enum(["MAW"]),
         "psrType": nw.Enum([key.name for key in PsrType]),
         "resolution": nw.Enum(["PT60M", "PT30M", "PT15M"]),
@@ -39,7 +39,7 @@ ACTUAL_GENERATION_PER_GENERATION_UNIT = nw.Schema(
 INSTALLED_CAPACITY_PER_PRODUCTION_TYPE = nw.Schema(
     {
         "timestamp": nw.Datetime(time_zone=UTC),
-        "quantity": nw.Int64,
+        "quantity": nw.Int64(),
         "quantity_Measure_Unit.name": nw.Enum(["MAW"]),
         "psrType": nw.Enum([key.name for key in PsrType]),
         "resolution": nw.Enum(["P1Y"]),
